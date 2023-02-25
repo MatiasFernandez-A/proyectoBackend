cf5a25a16dd8436a40ee65f9d7d9be28ffbedcf1
Matias Fernandez Almeda <fernandezalmedamatias@gmail.com>
2023-02-25 00:27:56 -0300

    Cambio ciclo forEach por findIndex

--- src/CartsManager.js
+++ src/CartsManager.js
@@ -7,13 +7,13 @@ const fileName = dirName + "carrito.json"
 
 
 class CartManager {
-    constructor (){
+    constructor() {
         this.path = fileName;
-    }   
-    createCart = async() =>{
+    }
+    createCart = async () => {
         let cart = await fs.promises.readFile(this.path, "utf-8")
         let cartParsed = JSON.parse(cart);
-        const cartId = cartParsed.length+1;
+        const cartId = cartParsed.length + 1;
         let newCarrito = {
             id: cartId,
             products: []
@@ -21,27 +21,57 @@ class CartManager {
         cartParsed.push(newCarrito)
         await fs.promises.writeFile(this.path, JSON.stringify(cartParsed))
     }
-    findCart = async(cid) => {
-        let cart = await fs.promises.readFile(this.path, "utf-8"); 
+    findCart = async (cid) => {
+        let cart = await fs.promises.readFile(this.path, "utf-8");
         let cartParsed = JSON.parse(cart);
         const cartFound = cartParsed.find(cart => cart.id === cid)
         return cartFound.products;
     }
-    addProductToCart = async(cid, pid) => {
-        let cart = await fs.promises.readFile(this.path, "utf-8"); 
+    addProductToCart = async (cid, pid) => {
+        let cart = await fs.promises.readFile(this.path, "utf-8");
         let cartParsed = JSON.parse(cart);
         let newProd = {
             product: pid,
             quantity: 1
         }
+
+        /*         
         cartParsed.forEach(e => {
-            if(e.id === cid && e.products == []){/* Si le pongo && no crea al prod
-            pero si el product existe solo me lo incrementa cuando cambio la condicion a || */
+            if (e.id === cid && e.products == []) {
                 e.products.push(newProd)
-            } else if (e.id === cid && e.products !== []){
+                } else if (e.id === cid && e.products !== []) {
                 e.products[0].quantity++
-            }
-        });
+                }
+                }); */
+
+        const index = cartParsed.findIndex(cart => cart.id === cid)
+
+        /*         if (!cartParsed[index].products) {
+                    cartParsed[index].products.push(newProd)
+                } else {
+                    cartParsed[index].products[0].quantity++
+                }
+         */
+
+        const productAlready = cartParsed[index].products.find(product => product.product === pid);
+
+        if (productAlready) {
+            const prodIndex = cartParsed[index].products.findIndex(product => product.product === pid);
+            cartParsed[index].products[prodIndex].quantity += 1;
+        } else {
+            cartParsed[index].products.push(newProd)
+            console.log(cartParsed[index].products);
+            const prodIndex = cartParsed[index].products.findIndex(product => product.product === pid);
+            cartParsed[index].products[prodIndex].quantity = 1;
+        }
+
+
+
+
+
+
+
+
 
         await fs.promises.writeFile(this.path, JSON.stringify(cartParsed))
     }
